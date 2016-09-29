ab2bc229041ee30bc992f47227881399d6fcdeea
Valerio Pipolo <valep27@gmail.com>
2016-09-29 10:33:33 +0200

    Improved formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,29 +1,28 @@
-import media
 import fresh_tomatoes
+from media import Movie
 
 
 def get_movies():
-    m1 = media.Movie("Inception",
-    "http://www.impawards.com/2010/posters/inception.jpg",
-    "https://www.youtube.com/watch?v=8hP9D6kZseM")
+    # Builds the list of movies to generate the site with
 
-    m2 = media.Movie("Shaun of the dead",
-    "http://www.impawards.com/2004/posters/shaun_of_the_dead_ver2_xlg.jpg",
-    "https://www.youtube.com/watch?v=LIfcaZ4pC-4")
+    m1 = Movie("Inception",
+               "http://www.impawards.com/2010/posters/inception.jpg",
+               "https://www.youtube.com/watch?v=8hP9D6kZseM")
 
-    m3 = media.Movie("The grand Budapest hotel",
-    "http://www.impawards.com/2014/posters/grand_budapest_hotel_ver17.jpg",
-    "https://www.youtube.com/watch?v=1Fg5iWmQjwk")
+    m2 = Movie("Shaun of the dead",
+               "http://www.impawards.com/2004/posters/"
+               "shaun_of_the_dead_ver2_xlg.jpg",
+               "https://www.youtube.com/watch?v=LIfcaZ4pC-4")
+
+    m3 = Movie("The grand Budapest hotel",
+               "http://www.impawards.com/2014/posters/"
+               "grand_budapest_hotel_ver17.jpg",
+               "https://www.youtube.com/watch?v=1Fg5iWmQjwk")
 
     movies = [m1, m2, m3]
-
     return movies
 
-
-def main():
+# Entry point of the program
+if __name__ == '__main__':
     movies = get_movies()
     fresh_tomatoes.open_movies_page(movies)
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(int(main() or 0))
